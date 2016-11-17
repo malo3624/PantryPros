62fe28f64759e99a78423f831d0ff56c3b1b764b
Bryan <copperclock@gmail.com>
2016-11-17 15:04:56 -0700

    Added a colon to fix a bug I had

--- django_project/hello/views.py
+++ django_project/hello/views.py
@@ -6,5 +6,5 @@ def index(request):
 	return render(request, 'hello/home.html')
 def about(request):
 	return render(request, 'hello/about.html')
-def contact(request)
+def contact(request):
 	return render(request, 'hello/contact.html')
